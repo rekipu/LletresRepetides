7d916c5a8f0f9ab4676c458d9f33e16b1ee89264
Joan Manel Ardura Terra <jarduraterra@gmail.com>
2019-07-08 12:21:18 +0200

    fase 1 correcta

--- src/Fase1.java
+++ src/Fase1.java
@@ -1,4 +1,13 @@
 
 public class Fase1 {
-
+	public static void main(String args[]) {
+		String name= "Joan Manel Ardura Terra";
+		char[] lletres = new char[name.length()];
+		for (int i=0; i<name.length();i++) {
+			lletres[i]=name.charAt(i);
+			System.out.println(lletres[i]);
+		}
+				
+	}
 }
+
