26449ce41b373e27197b237263988d9741d1aff7
Joan Manel Ardura Terra <jarduraterra@gmail.com>
2019-07-09 09:34:32 +0200

    fase 2 correcta

--- src/Fase2.java
+++ src/Fase2.java
@@ -7,8 +7,21 @@ public class Fase2 {
 		String name= "Joan Manel Ardura Terra";
 		List<Character> nameList = new ArrayList<Character>();
 		for (int i=0; i<name.length();i++) {
-			nameList.add(name.charAt(i));
-			System.out.println(nameList.get(i));
+			char caracter=name.charAt(i);
+			nameList.add(caracter);
+			if(Character.isDigit(caracter)) {
+				System.out.println("Els noms de persones no contenen números!");	
+			}else if(caracter=='a'||caracter=='e'||caracter=='i'||caracter=='o'||caracter=='u'||caracter=='A'||caracter=='E'||caracter=='I'||caracter=='O'||caracter=='U'){
+				System.out.println("VOCAL!");
+			}else if(caracter==' '){
+				System.out.println("ESPASIU!");
+			}else {
+				System.out.println("CONSONANT!");
+			}
+
+				
+			
+			System.out.println(caracter);
 		}
 				
 	}
